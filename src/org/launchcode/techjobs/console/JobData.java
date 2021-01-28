cf22d48e5892018a9f718d960a781901880d0ed7
Chen <batabaev.c@gmail.com>
2021-01-28 02:58:06 -0600

    try again

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -74,13 +75,28 @@ public class JobData {
 
         for (HashMap<String, String> row : allJobs) {
 
-            String aValue = row.get(column);
+            String aValue = row.get(column).toLowerCase();
 
             if (aValue.contains(value)) {
                 jobs.add(row);
             }
         }
 
+        return jobs;
+    }
+
+    public static ArrayList<HashMap<String, String>> findByValue(String value) {
+        loadData();
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        for (HashMap<String, String> job : allJobs) {
+            for (Map.Entry<String, String> values : job.entrySet()) {
+                String aValue = values.getValue().toLowerCase();
+                if (aValue.contains(value)) {
+                    jobs.add(job);
+                    break;
+                }
+            }
+        }
         return jobs;
     }
 
